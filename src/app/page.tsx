6d8a8be08b36a8b3799c6e8f1f86213625b3c212
MaxwellWhoSquats <maxwelleley2@gmail.com>
2024-12-13 19:49:11 -0700

    add credit (link to personal github)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,15 @@ const Dashboard = () => {
     <>
       <Navbar />
       <GameBar />
+      <p id="credit" className="bottom-0 absolute text-sm">
+        Created by:{" "}
+        <a
+          href="https://github.com/MaxwellWhoSquats"
+          className="text-blue-400 hover:text-base hover:text-blue-600"
+        >
+          Maxwell Eley
+        </a>
+      </p>
     </>
   );
 };
